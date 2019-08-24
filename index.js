d2f2d036bb757940987a24606384215f3d762c31
kimsphere <kimiley90@gmail.com>
2019-08-25 00:08:10 +0800

    changed index

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express   = require('express')
+const express   = require('express');
 const app       = express();
 const Discord   = require('discord.js');
 const client    = new Discord.Client();
