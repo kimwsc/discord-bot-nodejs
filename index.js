aad7346f7d1119adbdd42614e1b92181a5ec095d
kimsphere <kimiley90@gmail.com>
2019-09-02 19:51:09 +0800

    quick fix

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ client.on('guildMemberAdd', member => {
 // Create an event listener for new guild members
 client.on('guildMemberAdd', member => {
     // Send the message to a designated channel on a server:
-    const channel = member.guild.channels.find(ch => ch.name === '一般');
+    const channel = member.guild.channels.find(ch => ch.name === 'chat');
     // Do nothing if the channel wasn't found on this server
     if (!channel) return;
     // Send the message, mentioning the member
